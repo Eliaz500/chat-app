596817d1b49b8982c3fb2058fd35e8f6c72e9334
Eliaz500 <eliazzlima@gmail.com>
2025-01-11 13:49:49 -0300

    ChatHead configurado

--- src/componets/ChatHeader/index.js
+++ src/componets/ChatHeader/index.js
@@ -1,8 +1,23 @@
 import react from "react";
+import * as C from "./styles"
+import { MdPerson, MdMoreVert, MdSearch } from "react-icons/md";
 
 
-const ChatHeader = () => {
-    return <div>ChatHeader</div>
+const ChatHeader = ({ photoURL, nome}) => {
+    return (
+        <C.Container>
+            <C.UserInfo>
+                {photoURL ? (<C.Avatar src={photoURL} alt="Avatar"/>) : (<MdPerson />)}
+                <C.NomeContent>
+                    <C.Nome>{nome}</C.Nome>
+                </C.NomeContent>
+            </C.UserInfo>
+            <C.Options>
+                <MdSearch />
+                <MdMoreVert />
+            </C.Options>
+        </C.Container>
+    );
 };
 
 export default ChatHeader;
